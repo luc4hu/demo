634dabdbec5d1f6c804a0407769530571e167777
luc4hu <e11905159@student.tuwien.ac.at>
2024-09-28 21:46:14 +0200

    init

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System.Globalization;
+using CsvHelper;
+using CsvHelper.Configuration;
+
+namespace cons;
+
+internal static class Program
+{
+    private static async Task Main()
+    {
+        var csvConfig = new CsvConfiguration(CultureInfo.InvariantCulture)
+        {
+            NewLine = "\n"
+        };
+
+        using var reader = new StreamReader("/home/luca/projects/cons/db.csv");
+        await using var writer = new StreamWriter("/home/luca/projects/cons/csharp.csv");
+        using var csvReader = new CsvReader(reader, csvConfig);
+        await using var csvWriter = new CsvWriter(writer, csvConfig);
+
+        // Read header
+        await csvReader.ReadAsync();
+        csvReader.ReadHeader();
+        int headerLength = csvReader.HeaderRecord!.Length;
+        
+        // Write header
+        foreach (string header in csvReader.HeaderRecord)
+        {
+            csvWriter.WriteField(header);
+        }
+        await csvWriter.NextRecordAsync();
+        
+        // Read rest
+        while (await csvReader.ReadAsync())
+        {
+            for (int i = 0; i < headerLength; i++)
+            {
+                string? field = csvReader.GetField(i);
+                var shouldQuote = field != "(null)";
+                
+                if (Guid.TryParse(field, out Guid _))
+                {
+                    csvWriter.WriteField(Guid.NewGuid().ToString(), shouldQuote);
+                }
+                else
+                {
+                    csvWriter.WriteField(field, shouldQuote);
+                }
+            }
+
+            await csvWriter.NextRecordAsync();
+        }
+    }
+}
